13c8c4b40aeea1cc01e7d66fceaf98372aef2ad7
meso272 <liujinyang1201@126.com>
2021-12-05 12:29:14 -0800

    111

--- model.py
+++ model.py
@@ -295,6 +295,8 @@ def train(x,e):
         
         h_pred,memo = frame_predictor((h,memo))
         x_pred = encoder(h_pred,False)
+        print(x_pred.shape)
+        print(x[i].shape)
         mse +=  (mse_criterion(x_pred, x[i]))
 
 
