c469c409eec23bc07016d97dcea4a7559fc64125
meso272 <liujinyang1201@126.com>
2021-12-05 12:36:29 -0800

    111

--- utils_3d.py
+++ utils_3d.py
@@ -29,7 +29,7 @@ class psi(nn.Module):
         s_width = int(d_width * self.block_size)
         s_height = int(d_height * self.block_size)
         t_1 = output.contiguous().view(batch_size, temp, d_height, d_width, self.block_size_sq, s_depth)#4
-        spl = t_1.split(2, 4)
+        spl = t_1.split(self.block_size, self.block_size_sq)#2,4
         stack = [t_t.contiguous().view(batch_size, temp, d_height, s_width, s_depth) for t_t in spl]
         output = torch.stack(stack, 0).transpose(0, 1).transpose(1, 2).permute(0, 1, 3, 2, 4, 5).contiguous().view(batch_size, temp, s_height, s_width, s_depth)
         output = output.permute(0, 4, 1, 2, 3)
